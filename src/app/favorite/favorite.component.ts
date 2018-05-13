4efe502d7b21a9039dde4b08934e2dcfb8758c0a
prescott breeden <pb@prescotts-iMac.local>
2018-05-13 12:01:49 -0700

    created multi-star rating that outputs a value

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -6,13 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
-  isFavorite: boolean;
+  // isFavorite: boolean;
+  numStars;
+  oneStar: boolean;
+  twoStar: boolean;
+  threeStar: boolean;
+  fourStar: boolean;
+  fiveStar: boolean;
   constructor() { }
 
   ngOnInit() {
   }
 
-  toggleFavorite() {
-    this.isFavorite = !this.isFavorite;
+  toggleFavorite($event) {
+    this.numStars = $event.target.attributes.value.value;
+    this.oneStar = ('1' <= this.numStars);
+    this.twoStar = ('2' <= this.numStars);
+    this.threeStar = ('3' <= this.numStars);
+    this.fourStar = ('4' <= this.numStars);
+    this.fiveStar = ('5' <= this.numStars);
   }
 }
