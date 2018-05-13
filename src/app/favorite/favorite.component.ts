e30ad428e8f449cb4ab6011be9800a8482299f61
prescott breeden <pb@prescotts-iMac.local>
2018-05-13 03:10:58 -0700

    small refactor for favorite star (single span)

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
-  isFavorite;
+  isFavorite: boolean;
   constructor() { }
 
   ngOnInit() {
