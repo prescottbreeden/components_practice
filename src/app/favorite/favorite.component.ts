0a3067373bf1bdca992a78af88e273dc72626ab9
prescott breeden <pb@prescotts-iMac.local>
2018-05-13 03:01:02 -0700

    made a dynamic 'favorite' with star

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+  isFavorite;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite;
+  }
+}
